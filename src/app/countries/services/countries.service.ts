c3b28c597de150fdccd37ea6962b57d399ba71e1
eduardo 34 - pc Efrain <eduardomelchor34@gmail.com>
2024-07-14 17:40:02 -0600

    Buscar por pais y region

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -22,5 +22,27 @@ export class CountriesService {
 
   }
 
+  searchCountry( term: string ): Observable<Country[]> {
+
+    const url = `${ this.apiUrl }/name/${ term }`;
+
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( () => of([]) )
+      );
+
+  }
+
+  searchRegion( region: string ): Observable<Country[]> {
+
+    const url = `${ this.apiUrl }/region/${ region }`;
+
+    return this.http.get<Country[]>( url )
+      .pipe(
+        catchError( () => of([]) )
+      );
+
+  }
+
 
 }
