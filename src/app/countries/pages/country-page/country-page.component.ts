fbacea02fbf51654fab71d0aff920839e546ce6a
eduardo 34 - pc Efrain <eduardomelchor34@gmail.com>
2024-07-14 19:03:49 -0600

    swtichMap - Refactorizacion

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -11,20 +12,25 @@ export class CountryPageComponent implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private countriesService: CountriesService
+    private router: Router,
+    private countriesService: CountriesService,
   ) {}
 
   ngOnInit(): void {
     this.activatedRoute.params
-      .subscribe( ({ id }) => {
+      .pipe(
+        switchMap( ({ id }) => this.countriesService.searchCountryByAlphaCode( id ) ),
+      )
+      .subscribe( country => {
 
-        this.countriesService.searchCountryByAlphaCode( id )
-          .subscribe( country => {
-            console.log({ country })
-          });
+        if( !country ) {
+          return this.router.navigateByUrl('');
+        }
 
-      });
+        console.log('tenemos un pais');
+        return;
+
+    });
   }
-
 
 }
